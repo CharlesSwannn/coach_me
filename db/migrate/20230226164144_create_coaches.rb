bf1689fb56ecee3bd1620211a8dd9ebf14ad6ba9
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-02-26 16:42:22 +0000

    create coach model

---
+++ db/migrate/20230226164144_create_coaches.rb
@@ -0,0 +1,11 @@
+class CreateCoaches < ActiveRecord::Migration[7.0]
+  def change
+    create_table :coaches do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.string :details
+
+      t.timestamps
+    end
+  end
+end
