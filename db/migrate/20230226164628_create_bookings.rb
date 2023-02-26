6cd3c6a5dd0bb3522fd9410fb66a507aa23e7006
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-02-26 16:51:41 +0000

    created review and booking model

---
+++ db/migrate/20230226164628_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :coach, null: false, foreign_key: true
+      t.string :location
+      t.datetime :start_time
+      t.datetime :end_time
+
+      t.timestamps
+    end
+  end
+end
